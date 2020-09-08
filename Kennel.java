449ca2955ead46006ed7b5df8dce20d2bce269dd
TinyClayMan <70881505+TinyClayMan@users.noreply.github.com>
2020-09-08 10:25:00 +0300

    Update Kennel.java

--- Kennel.java
+++ Kennel.java
@@ -1,9 +1,22 @@
 package com.company;
 
 public class Kennel {
-    private String[] dogs= new String[];
+    private int dogsCount;
+    Dog[] dogs;
 
-    public Kennel(){}
+    public Kennel(int amount){
+        this.dogsCount = 0;
+        dogs = new Dog[amount];
+    }
 
-    public void addDog(Dog dog);
+    public void addDogs(Dog ... dogs){
+        for (int i=0; i<dogs.length; i++){
+            this.dogs[dogsCount] = dogs[i];
+            dogsCount++;
+        }
+    }
+    
+    public Dog getDog(int i){
+        return dogs[i];
+    }
 }
